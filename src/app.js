7e667584f7b83c624f40ba712461ba3d838f7c2d
joao <joao.dev94@gmail.com>
2023-02-27 10:32:18 -0300

    Inicio do projeto

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Inicio do projeto
 const express = require('express');
 
 const app = express();
@@ -9,5 +10,5 @@ app.get('/', (_request, response) => {
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
